60d0d29889f55f535dcf8e6049866675bb622832
Abdullah Asif <124244121+Abdullah-056@users.noreply.github.com>
2023-04-08 16:50:07 +0500

    Simpson 1/3 rule

---
+++ simp.m
@@ -0,0 +1,29 @@
+syms x
+% Lower Limit
+a = 4;
+% Upper Limit
+b = 5.2;
+% Number of Segments
+n = 6;
+% Declare the function
+f1 = log(x);
+% inline creates a function of string containing in f1
+f = inline(f1);
+% h is the segment size
+h = (b - a)/n;
+% X stores the summation of first and last segment
+X = f(a)+f(b);
+Odd = 0;
+Even = 0;
+for i = 1:2:n-1
+    xi=a+(i*h);
+    Odd=Odd+f(xi);
+end
+for i = 2:2:n-2
+    xi=a+(i*h);
+    Even=Even+f(xi);
+end
+% Formula to calculate numerical integration using Simpsons 1/3 Rule
+I = (h/3)*(X+4*Odd+2*Even);
+disp('The approximation of above integral is: ');
+disp(I);
